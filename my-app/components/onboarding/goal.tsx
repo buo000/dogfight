2c0f806a82654ca18cc44f53e4121b7b3c5444b5
buo <306004921+buo000@users.noreply.github.com>
2026-09-13 15:25:31 -0400

    Remove dont yap from goal question

--- my-app/components/onboarding/goal.tsx
+++ my-app/components/onboarding/goal.tsx
@@ -1,7 +1,7 @@
 export default function Goal({setGoal}: {setGoal: (value: string) => void;}) {
     return (
         <div className="flex flex-col gap-5 items-start">
-            <label htmlFor="">What is your goal?<small>dont yap</small></label>
+            <label htmlFor="">What is your goal?</label>
             <textarea onChange={(e) => setGoal(e.target.value)} className="border-2 border-black p-3 outline-none min-h-48 max-h-48" name="" id="" maxLength={68} placeholder={"become a\ncompetent and job\nready full stack\nweb developer"}></textarea>
         </div>
     );
